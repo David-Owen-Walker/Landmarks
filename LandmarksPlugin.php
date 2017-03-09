0f754ea8431728496f9e431d9993ddb82cc8bd8c
David Walker <walk.dv@gmail.com>
2017-03-09 15:37:55 -0400

    skeleton code from TourBuilder

---
+++ LandmarksPlugin.php
@@ -0,0 +1,194 @@
+<?php
+
+if( !defined( 'TOURBUILDER_PLUGIN_DIR' ) )
+{
+	define( 'TOURBUILDER_PLUGIN_DIR', dirname( __FILE__ ) );
+}
+
+class TourBuilderPlugin extends Omeka_Plugin_AbstractPlugin
+{
+	protected $_filters = array(
+        'api_resources',
+//		'public_navigation_main',
+//		'admin_dashboard_stats',
+//		'admin_navigation_main' 
+    );
+
+	protected $_hooks = array(
+//		'install',
+//		'uninstall',
+//		'define_acl',
+//		'define_routes',
+//		'admin_head',
+//		'admin_dashboard',
+//		'upgrade',
+	);
+
+//	public function hookInstall()
+//	{
+//		$db = $this->_db;
+//
+//		$tourQuery = "
+//         CREATE TABLE IF NOT EXISTS `$db->Tour` (
+//            `id` int( 10 ) unsigned NOT NULL auto_increment,
+//            `title` varchar( 255 ) collate utf8_unicode_ci default NULL,
+//            `description` text collate utf8_unicode_ci NOT NULL,
+//            `credits` text collate utf8_unicode_ci,
+//            `tour_image` text collate utf8_unicode_ci,
+//            `postscript_text` text collate utf8_unicode_ci,
+//            `featured` tinyint( 1 ) default '0',
+//            `public` tinyint( 1 ) default '0',
+//            `slug` varchar( 30 ) collate utf8_unicode_ci default NULL,
+//            PRiMARY KEY( `id` ),
+//            UNIQUE KEY `slug` ( `slug` )
+//         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ";
+//
+//		$tourItemQuery = "
+//         CREATE TABLE IF NOT EXISTS `$db->TourItem` (
+//            `id` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT,
+//            `tour_id` INT( 10 ) UNSIGNED NOT NULL,
+//            `ordinal` INT NOT NULL,
+//            `item_id` INT( 10 ) UNSIGNED NOT NULL,
+//            PRIMARY KEY( `id` ),
+//            KEY `tour` ( `tour_id` )
+//         ) ENGINE=InnoDB ";
+//
+//		$db->query( $tourQuery );
+//		$db->query( $tourItemQuery );
+//	}
+//
+//	public function hookUninstall()
+//	{
+//		$db = $this->_db;
+//		$db->query( "DROP TABLE IF EXISTS `$db->TourItem`" );
+//		$db->query( "DROP TABLE IF EXISTS `$db->Tour`" );
+//	}
+
+//    public function hookUpgrade($args)
+//    {
+//        $oldVersion = $args['old_version'];
+//        $newVersion = $args['new_version'];
+//        $db = $this->_db;
+//        
+//        if ($oldVersion < '1.4') {
+//            
+//            $sql = "ALTER TABLE `$db->Tour` ADD COLUMN `postscript_text` text collate utf8_unicode_ci default NULL";
+//            $db->query($sql);
+//            
+//            $sql = "ALTER TABLE `$db->Tour` ADD COLUMN `tour_image` text collate utf8_unicode_ci default NULL";
+//            $db->query($sql);            	        
+//	    }
+//	}
+	
+//	public function hookDefineAcl( $args )
+//	{
+//		$acl = $args['acl'];
+//
+//		// Create the ACL context
+//		$acl->addResource( 'TourBuilder_Tours' );
+//		
+//		// Allow anyone to look but not touch
+//		$acl->allow( null, 'TourBuilder_Tours', array('browse', 'show') );
+//		
+//		// Allow contributor (and better) to do anything with tours
+//		$acl->allow( 'contributor','TourBuilder_Tours');
+//
+//	}
+
+//	public function hookDefineRoutes( $args )
+//	{
+//		$router = $args['router'];
+//		$router->addConfig( new Zend_Config_Ini(
+//				TOURBUILDER_PLUGIN_DIR .
+//				DIRECTORY_SEPARATOR .
+//				'routes.ini', 'routes' ) );
+//	}
+
+//	public function filterAdminDashboardStats( $stats )
+//	{
+//		if( is_allowed( 'TourBuilder_Tours', 'browse' ) )
+//		{
+//			$stats[] = array( link_to( 'tours', array(),
+//					total_records( 'Tours' ) ),
+//				__('tours') );
+//		}
+//		return $stats;
+//	}
+
+//	public function filterPublicNavigationMain( $navs )
+//	{
+//		$navs[] = array(
+//			'label' => __('Tours'),
+//			'uri' => url( 'tours' ),
+//			'visible' => true
+//		);
+//		return $navs;
+//	}
+
+//	public function hookAdminDashboard()
+//	{
+//		// Get the database.
+//		$db = get_db();
+//
+//		// Get the Tour table.
+//		$table = $db->getTable('Tour');
+//
+//		// Build the select query.
+//		$select = $table->getSelect();
+//
+//		// Fetch some items with our select.
+//		$results = $table->fetchObjects($select);
+//
+//		$tourItems = null;
+//		$html  = null;
+//
+//		for($i=0;$i<=5;$i++){
+//			if(array_key_exists($i,$results) && is_object($results[$i])){
+//				$tourItems .='<div class="recent-row"><p class="recent"><a href="/admin/tours/show/'.$results[$i]->id.'">'
+//					.$results[$i]->title.'</a></p><p class="dash-edit"><a href="/admin/tours/edit/'.$results[$i]->id.'">Edit</a></p></div>';
+//			}
+//		}
+//
+//		$html .= '<section class="five columns alpha"><div class="panel">';
+//		$html .= '<h2>'.__('Recent Tours').'</h2>';
+//		$html .= ''.$tourItems.'';
+//		$html .= '<p><a class="add-new-item" href="'.html_escape(url('tour-builder/tours/add/')).'">'.__('Add a new tour').'</a></p>';
+//		$html .= '</div></section>';
+//
+//		echo $html;
+//
+//	}
+
+
+
+
+
+//	public function hookAdminHead()
+//	{
+//		queue_css_file('tour');
+//	}
+
+	/**
+     * Register the landmarks API resource.
+     *
+     * @param array $apiResources to add to
+     * @return array the passed in array, with landmarks added
+     */
+    public function filterApiResources($apiResources)
+    {
+        $apiResources['landmarks'] = array(
+            'record_type' => 'Landmark', # There is no Landmark record
+            'actions' => array('index'),
+            'index_params' => array('near')
+        );
+        return $apiResources;
+    }
+	
+//	public function filterAdminNavigationMain( $nav )
+//	{
+//		$nav['Tours'] = array( 'label' => __('Tours'),
+//			'action' => 'browse',
+//			'controller' => 'tours' );
+//		return $nav;
+//	}
+}
---
+++ models/Landmark.php
@@ -0,0 +1,214 @@
+<?php
+
+require_once 'LandmarkTable.php';
+
+/**
+ * Tour
+ * @package: Omeka
+ */
+class Landmark extends Omeka_Record_AbstractRecord implements Zend_Acl_Resource_Interface
+//{
+//	public $title;
+//	public $description;
+//	public $credits;
+//	public $featured = 0;
+//	public $public = 0;
+//	public $slug;
+//	public $postscript_text;
+//	public $tour_image;
+//
+//	protected $_related = array( 'Items' => 'getItems','Image' => 'getImage' );
+    
+    /**
+     * Identify Tour records as relating to the Tours ACL resource.
+     * 
+     * @return string
+     */
+    public function getResourceId()
+    {
+        return 'Landmarks';
+    }
+
+//	public function getItems()
+//	{
+//		return $this->getTable()->findItemsByTourId( $this->id );
+//	}
+//
+//
+//	public function removeAllItems( ) {
+//		$db = get_db();
+//		$tiTable = $db->getTable( 'TourItem' );
+//		$select = $tiTable->getSelect();
+//		$select->where( 'tour_id = ?', array( $this->id ) );
+//
+//		# Get the tour item
+//		$tourItems = $tiTable->fetchObjects( $select );
+//
+//		# Iterate through all the tour items
+//		# and remove them
+//		for($i = 0; $i < count($tourItems); $i++) {
+//			$tourItems[$i]->delete();
+//		}
+//	}
+//
+//	public function removeItem( $item_id )
+//	{
+//		if( !is_numeric( $item_id ) ) {
+//			$item_id = $item_id->id;
+//		}
+//
+//		# First get the tour-item object
+//		$db = get_db();
+//		$tiTable = $db->getTable( 'TourItem' );
+//		$select = $tiTable->getSelect();
+//		$select->where( 'tour_id = ?', array( $this->id ) )
+//		->where( 'item_id = ?', array( $item_id ) );
+//
+//		# Get the tour item
+//		$tourItem = $tiTable->fetchObject( $select );
+//
+//		# Renumber any ordinals greater than it.
+//		$select = $tiTable->getSelect();
+//		$select->where( 'tour_id = ?', array( $this->id ) )
+//		->where( 'ordinal > ?', array( $tourItem->ordinal ) );
+//
+//		# Delete this linkage
+//		$tourItem->delete();
+//
+//		# Reorder the remaining linkages
+//		$renumbers = $tiTable->fetchObjects( $select );
+//		foreach( $renumbers as $ti )
+//		{
+//			$ti->ordinal = $ti->ordinal - 1;
+//			$ti->save();
+//		}
+//
+//	}
+//
+//	public function addItem( $item_id, $ordinal = null )
+//	{
+//		if( !is_numeric( $item_id ) ) {
+//			$item_id = $item_id->id;
+//		}
+//
+//		# Get the next ordinal
+//		$db = get_db();
+//		$tiTable = $db->getTable( 'TourItem' );
+//		$select = $tiTable->getSelectForCount();
+//		$select->where( 'tour_id = ?', array( $this->id ) );
+//		if($ordinal === null) {
+//			$ordinal = $tiTable->fetchOne( $select );
+//		}
+//
+//		# Create, assign, and save the new tour item connection
+//		$tourItem = new TourItem;
+//		$tourItem->tour_id = $this->id;
+//		$tourItem->item_id = $item_id;
+//		$tourItem->ordinal = $ordinal;
+//		$tourItem->save();
+//	}
+//
+//	public function saveItemOrder( $tour_id ) {
+//
+//	}
+//
+//	public function hoistItem( $tour_id, $item_id )
+//	{
+//		$this->swapItem( $tour_id, $item_id, true );
+//	}
+//
+//	public function lowerItem( $tour_id, $item_id )
+//	{
+//		$this->swapItem( $tour_id, $item_id, false );
+//	}
+//
+//	public function setItemOrdinal( $tour_id, $item_id, $ordinal ) {
+//		$db = get_db();
+//		$tiTable = $db->getTable( 'TourItem' );
+//
+//		// Get the target item
+//		$select = $tiTable->getSelect()
+//		->where( 'tour_id = ?', $tour_id )
+//		->where( 'item_id = ?', $item_id );
+//		$item = $tiTable->fetchObject( $select );
+//		$item->ordinal = $ordinal;
+//		$item->save();
+//	}
+//
+//	public function swapItem( $tour_id, $item_id, $up )
+//	{
+//		$db = get_db();
+//		$tiTable = $db->getTable( 'TourItem' );
+//
+//		// Get the target item
+//		$select = $tiTable->getSelect()
+//		->where( 'tour_id = ?', $tour_id )
+//		->where( 'item_id = ?', $item_id );
+//		$left = $tiTable->fetchObject( $select );
+//		$ordinal = intval( $left->ordinal );
+//
+//		// Get the next item with which we are swapping
+//		$select = $tiTable->getSelect()
+//		->where( 'tour_id = ?', $tour_id )
+//		->where( $up ? 'ordinal < ?' : 'ordinal > ?', $ordinal )
+//		->limit( 1 );
+//		$right = $tiTable->fetchObject( $select );
+//
+//		// Do the ordinal shuffle
+//		$left->ordinal = intval( $right->ordinal );
+//		$right->ordinal = $ordinal;
+//
+//		// Save both items
+//		$left->save();
+//		$right->save();
+//	}
+//
+//	protected function _validate()
+//	{
+//		if( empty( $this->title ) ) {
+//			$this->addError( 'title', 'Tour must be given a title.' );
+//		}
+//
+//		if( strlen( $this->title > 255 ) ) {
+//			$this->addError( 'title', 'Title for a tour must be 255 characters or fewer.' );
+//		}
+//		if (!$this->fieldIsUnique('title')) {
+//			$this->addError('title', 'The Title is already in use by another tour. Please choose another.');
+//		}
+//
+//		if( strlen( $this->slug > 30 ) ) {
+//			$this->addError( 'slug', 'Slug for a tour must be 30 characters or fewer.' );
+//		}
+//		
+//		if( !empty($this->tour_image) && !is_array(getimagesize( $this->tour_image )) ){
+//			$this->addError('tour_image','The text entered does not validate as an image URL.');
+//		}
+//
+//		if( empty( $this->slug ) ) {
+//			if($title=$this->title){
+//
+//			// replace non letter or digits by -
+//			$title = preg_replace('~[^\\pL\d]+~u', '-', $title);
+//			
+//			// trim
+//			$title = trim($title, '-');
+//			
+//			// transliterate
+//			$title = iconv('utf-8', 'us-ascii//TRANSLIT', $title);
+//			
+//			// lowercase
+//			$title = strtolower($title);
+//			
+//			// remove unwanted characters
+//			$title = preg_replace('~[^-\w]+~', '', $title);				
+//				
+//				$this->slug= $title;
+//			}else{
+//				$this->addError( 'slug', 'Tour must be given a slug.' );
+//			}
+//		}
+//		if (!$this->fieldIsUnique('slug')) {
+//			$this->addError('slug', 'The slug is already in use by another tour. Please choose another.');
+//		}
+//	}
+}
