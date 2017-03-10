cf34c37a74c45b8a746385085d165da899fd9fe8
David Walker <walk.dv@gmail.com>
2017-03-10 18:33:31 -0400

    trust in the heart of the cards

--- models/Api/Landmark.php
+++ models/Api/Landmark.php
@@ -73,8 +73,8 @@ class Api_Landmark extends Omeka_Record_Api_AbstractRecordAdapter
             'url' => self::getResourceUrl("/files?item={$record->id}"),
             'resource' => 'files',
         );
-//        $representation['tags'] = $this->getTagRepresentations($record);
-//        $representation['element_texts'] = $this->getElementTextRepresentations($record);
+        $representation['tags'] = $this->getTagRepresentations($record);
+        $representation['element_texts'] = $this->getElementTextRepresentations($record);
         $representation['debug'] = get_object_vars($record);
         return $representation;
     }
