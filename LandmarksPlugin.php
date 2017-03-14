4343fd42b49ac7726bca781f10efc2b4799508a7
David Walker <walk.dv@gmail.com>
2017-03-14 11:39:25 -0300

    extended resources

--- LandmarksPlugin.php
+++ LandmarksPlugin.php
@@ -178,7 +178,7 @@ class LandmarksPlugin extends Omeka_Plugin_AbstractPlugin
     public function filterApiResources($apiResources)
     {
         $apiResources['landmarks'] = array(
-            'controller' => 'landmarks-controller',
+            'controller' => 'landmarksController',
             'record_type' => 'Landmark',
             'actions' => array('get', 'index'),
             'index_params' => array('near')
