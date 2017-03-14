28e21ea88168a8064c75335c1f5c77fbd0f95c3b
David Walker <walk.dv@gmail.com>
2017-03-14 10:59:29 -0300

    extended resources

--- controllers/LandmarksController.php
+++ controllers/LandmarksController.php
@@ -8,8 +8,35 @@ class LandmarksController extends Omeka_Controller_AbstractActionController
 	{
 		$this->_helper->db->setDefaultModelName( 'Landmark' );
 	}
+    
+    
+    protected function _getRepresentation(
+        Omeka_Record_Api_AbstractRecordAdapter $recordAdapter,
+        Omeka_Record_AbstractRecord $record,
+        $resource
+    ) {
+        $extend = array();
+        $extendTemp = apply_filters("api_extend_items", array(), array('record' => $record));
+        $apiResources = $this->getFrontController()->getParam('api_resources');
+        // Validate each extended resource. Each must be registered as an API
+        // resource and the content must contain "id" and "url" for one resource
+        // or "count" and "url" for multiple resources. A "resource" is
+        // recommended but not mandatory. Everything else passes through as
+        // custom data that may be used for the client's convenience.
+        foreach ($extendTemp as $extendResource => $extendContent) {
+            if (is_array($extendContent)
+                && array_key_exists($extendResource, $apiResources)
+                && (array_key_exists('count', $extendContent) || array_key_exists('id', $extendContent))
+                && array_key_exists('url', $extendContent)
+            ) {
+                $extend[$extendResource] = $extendContent;
+            }
+        }
+        
+        $representation['extended_resources'] = $extend;
+    }
 
-	public function removeitemAction()
+    public function removeitemAction()
 	{
 		// Get the landmark and item id from the request
 		$landmark = $this->_helper->db->findById();
