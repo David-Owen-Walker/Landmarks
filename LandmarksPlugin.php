3a109caae81e01cd81ea2010cce23097429357b7
David Walker <walk.dv@gmail.com>
2017-03-28 15:14:34 -0300

    remove routes correctly

--- LandmarksPlugin.php
+++ LandmarksPlugin.php
@@ -10,8 +10,7 @@ class LandmarksPlugin extends Omeka_Plugin_AbstractPlugin
 	protected $_filters = array('api_resources');
 
 	protected $_hooks = array(
-		'define_acl',
-		'define_routes',
+		'define_acl'
 	);
 
 	public function hookDefineAcl( $args )
@@ -27,15 +26,6 @@ class LandmarksPlugin extends Omeka_Plugin_AbstractPlugin
 		// Allow contributor (and better) to do anything with landmarks
 		$acl->allow( 'contributor','Landmarks');
 
-	}
-
-	public function hookDefineRoutes( $args )
-	{
-		$router = $args['router'];
-		$router->addConfig( new Zend_Config_Ini(
-				LANDMARKS_PLUGIN_DIR .
-				DIRECTORY_SEPARATOR .
-				'routes.ini', 'routes' ) );
 	}
 
 	public function filterPublicNavigationMain( $navs )
