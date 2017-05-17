08ec4a61e5cc69b94c70277c98849733da064cc4
David Owen Walker <walk.dv@gmail.com>
2017-05-17 18:44:22 -0300

    only get public landmarks

--- models/LandmarkTable.php
+++ models/LandmarkTable.php
@@ -26,7 +26,7 @@ class LandmarkTable extends Omeka_Db_Table
 			$alias = $this->getTableAlias();
 
 			$select->join(array("loc"=>$db->Location),
-                          "loc.item_id = " . $alias .".id AND ".$alias.".public != 0",
+                          "loc.item_id = " . $alias .".id AND ".$alias.".public = true",
                           array("distance"=> $distance));
 
 			$select->reset( Zend_Db_Select::ORDER );
